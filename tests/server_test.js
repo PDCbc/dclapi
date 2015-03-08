8e984fad6d7cd946f5e828a0db6d372d0e0fd77e
drusk <drusk@uvic.ca>
2015-03-07 19:28:19 -0800

    Added /classbyatc/:atc service.

--- tests/server_test.js
+++ tests/server_test.js
@@ -1,22 +1,38 @@
 var assert = require("assert");
+var sinon = require("sinon");
 var request = require("supertest");
 
+var database = require("../lib/database");
 var server = require("../lib/server");
 
 describe("Server", function() {
-    server.setup(null);
-    request = request(server.app);
+    describe("GET /classbyatc/", function() {
+        var db;
 
-    describe("GET /test", function() {
-        it("should return Hello World in JSON", function(done) {
-            request.get("/test")
+        beforeEach(function() {
+            db = sinon.createStubInstance(database.SQLiteInterface);
+            server.setup(db);
+        });
+
+        it("should return JSON containing description for a known ATC code", function(done) {
+            db.getAtcDescription = function(atcCode, callback) {
+                if (atcCode === "A") {
+                    callback(null, "ALIMENTARY TRACT AND METABOLISM");
+                }
+            };
+
+            request(server.app).get("/classbyatc/A")
                 .expect("Content-Type", "application/json")
                 .end(function(error, response) {
                     if (error) {
                         throw error;
                     }
 
-                    assert.deepEqual(JSON.parse(response.text), {message: "Hello world"});
+                    assert.deepEqual(
+                        JSON.parse(response.text),
+                        {ATC: "A", class: "ALIMENTARY TRACT AND METABOLISM"}
+                    );
+
                     done();
                 });
         });
