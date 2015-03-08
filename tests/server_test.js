c793032bdf6266251311cc9779cb476cbd186536
drusk <drusk@uvic.ca>
2015-03-08 16:34:16 -0700

    Added atcbydin web service.

--- tests/server_test.js
+++ tests/server_test.js
@@ -62,4 +62,61 @@ describe("Server", function() {
                 });
         });
     });
+
+    describe("GET /atcbydin/", function() {
+        var db;
+        var webService;
+
+        beforeEach(function() {
+            db = sinon.createStubInstance(database.SQLiteInterface);
+            webService = new server.ExpressServer(db);
+        });
+
+        it("should pad DIN and return ATC for known DIN", function(done) {
+            db.getAtcFromDin = function(din, callback) {
+                if (din === "02229377") {
+                    callback(null, "D08AC02");
+                }
+            };
+
+            request(webService.expressApp).get("/atcbydin/2229377")
+                .expect(200)
+                .expect("Content-Type", "application/json; charset=utf-8")
+                .end(function(error, response) {
+                    if (error) {
+                        throw error;
+                    }
+
+                    assert.deepEqual(
+                        JSON.parse(response.text),
+                        {DIN: "02229377", ATC: "D08AC02"}
+                    );
+
+                    done();
+                });
+        });
+
+        it("should return 404 and error message for an unknown DIN", function(done) {
+            var errorMessage = "DIN not recognized";
+            db.getAtcFromDin = function(din, callback) {
+                callback(new Error(errorMessage), null);
+            };
+
+            request(webService.expressApp).get("/atcbydin/123")
+                .expect(404)
+                .expect("Content-Type", "application/json; charset=utf-8")
+                .end(function(error, response) {
+                    if (error) {
+                        throw error;
+                    }
+
+                    assert.deepEqual(
+                        JSON.parse(response.text),
+                        {DIN: "00000123", error: errorMessage}
+                    );
+
+                    done();
+                });
+        });
+    });
 });
