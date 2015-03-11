938cd8f5f56685956363f742536ff5e21b996c1b
drusk <drusk@uvic.ca>
2015-03-10 22:37:46 -0700

    Added method to trim ATC codes to a specific level of the hierarchy.

--- tests/controllers_test.js
+++ tests/controllers_test.js
@@ -16,4 +16,37 @@ describe("controllers", function() {
             assert.strictEqual("00001234", controllers.padDIN("1234"));
         });
     });
+
+    describe("trimAtcCode", function() {
+        var atc;
+
+        beforeEach(function() {
+            atc = "D08AC02";
+        });
+
+        it("should return just the first letter for level 1", function() {
+            assert.strictEqual("D", controllers.trimAtcCode(atc, 1));
+        });
+
+        it("should return the first 3 characters for level 2", function() {
+            assert.strictEqual("D08", controllers.trimAtcCode(atc, 2));
+        });
+
+        it("should return the first 4 characters for level 3", function() {
+            assert.strictEqual("D08A", controllers.trimAtcCode(atc, 3));
+        });
+
+        it("should return the first 5 characters for level 4", function() {
+            assert.strictEqual("D08AC", controllers.trimAtcCode(atc, 4));
+        });
+
+        it("should return all 7 characters for level 5", function() {
+            assert.strictEqual("D08AC02", controllers.trimAtcCode(atc, 5));
+        });
+
+        it("should throw an error if the level is not between 1 and 5", function() {
+            assert.throws(function() { controllers.trimAtcCode(atc, 0); }, Error);
+            assert.throws(function() { controllers.trimAtcCode(atc, 6); }, Error);
+        });
+    });
 });
